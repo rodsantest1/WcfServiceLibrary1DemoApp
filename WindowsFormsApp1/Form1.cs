432db1a126a60161906cae7e6bb0972bdf5640e9
Rodney Santiago <rodney.santiago@ups.com>
2023-04-21 13:50:31 -0400

    Add project files.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(System.Object sender, System.EventArgs e)
+        {
+            ServiceReference1.Service1Client client = new
+                ServiceReference1.Service1Client();
+            string returnString;
+
+            returnString = client.GetData(textBox1.Text);
+            label1.Text = returnString;
+        }
+    }
+}
